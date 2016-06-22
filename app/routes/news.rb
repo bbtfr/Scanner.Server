9381cbcdb3d864b20edf9983978f1528dbc5bfc0
Theo Li <bbtfrr@gmail.com>
2016-06-22 13:06:00 +0800

    better news json format

--- app/routes/news.rb
+++ app/routes/news.rb
@@ -5,16 +5,19 @@ module Scanner
       set :root, File.expand_path("../../..", __FILE__)
 
       get '/page/:page' do
-        from = (params[:page] || 1).to_i * 10 - 9
+        page = (params[:page] || 1).to_i
+        from = page * 10 - 9
         to = from + 10
 
         base_url = "#{request.scheme}://#{request.host_with_port}"
-        (from..to).map do |i|
+        data = (from..to).map do |i|
           { title: "社保新闻 #{i}",
             subtitle: "#{Time.now.strftime("%Y年%-m月%-d日 %H:%M")} 李阳",
             url: "#{base_url}/news/sample.html",
             thumbnail: "#{base_url}/news/sample.jpg" }
-        end.to_json
+        end
+
+        render_json data: data, page: page
       end
 
     end
