0fe04a6b8d9774b3d2fd7c88423f7b9728bc3d79
Theo Li <bbtfrr@gmail.com>
2016-07-22 18:22:00 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :news
-  resource :audit, only: :show do
+  resource :audit, only: [] do
     get :identify
     get :people
+
+    get :show, to: redirect('/audit/identify')
   end
 
   namespace :api, defaults: { format: 'json' } do
@@ -15,4 +17,6 @@ Rails.application.routes.draw do
       post :use_count
     end
   end
+
+  root to: 'news#index'
 end
