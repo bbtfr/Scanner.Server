23f14647be15e1a58f325ade7cb7aa5fd0c02d42
Theo Li <bbtfrr@gmail.com>
2016-08-05 00:26:59 +0800

    add error message for name_and_idcard_number_doesn_t_match

--- app/models/identify_calling.rb
+++ app/models/identify_calling.rb
@@ -7,7 +7,8 @@ class IdentifyCalling < ApplicationRecord
   VALIDITIES = {
     '检验成功' => 'accepted',
     '脸谱信息验证失败' => 'ineligible_confidence',
-    '身份证信息验证失败' => 'invalid_idcard_number'
+    '身份证号码校验失败' => 'invalid_idcard_number',
+    '身份证信息验证失败' => 'name_and_idcard_number_doesn_t_match'
   }
 
   serialize :sensetime_result, Hash
