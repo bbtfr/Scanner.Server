ba0cd10cab7c088e6f0aeaf388d860b03d2ce06f
Theo Li <bbtfrr@gmail.com>
2016-08-06 21:11:04 +0800

    add searchable fields

--- app/models/identify_calling.rb
+++ app/models/identify_calling.rb
@@ -1,6 +1,6 @@
 class IdentifyCalling < ApplicationRecord
   ENDPOINTS = {
-    '公安身份证照片比对接口' => 'selfie_idnumber_verification',
+    '公安照片比对接口' => 'selfie_idnumber_verification',
     '人脸比对接口' => 'historical_selfie_verification'
   }
 
